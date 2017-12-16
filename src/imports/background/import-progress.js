72fb5b905bdf2106b0413e62edfd54ed29cab673
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-12-16 13:16:01 +0700

    Implement Promise concurrency at import item level
    - works with existing UI and state
    - updates token state to be variable length and store cancellation tokens for $concurrency_level Promises
    - seems to work alright
    - new pure JS script `promise-limit` added in to easily afford concurrent `Promise.all`-like behaviour (really nice compared with prev RxJS implementation)

--- src/imports/background/import-progress.js
+++ src/imports/background/import-progress.js
@@ -1,3 +1,5 @@
+import promiseLimit from 'promise-limit'
+
 import stateManager from './import-state'
 import processImportItem from './import-item-processor'
 
@@ -6,12 +8,17 @@ class ImportProgressManager {
     static CONCURR_LIMIT = 5
 
     /**
-     * @property {any} Token object to afford cancellation of currently running Promises.
+     * @property {any[]} Token objects with `cancel` method to afford cancellation of currently running Promises.
      */
-    token = {}
+    tokens = []
 
     /**
-     * @property {number} Level of concurrency for passing to `chunkProcessor`
+     * @property {number} Number between 0 and `this._concurrency` representing the latest scheduled job's index in `this.tokens`.
+     */
+    _tokenIndex = 0
+
+    /**
+     * @property {number} Currently set level of concurrency.
      */
     _concurrency
 
@@ -23,13 +30,38 @@ class ImportProgressManager {
     constructor(initConcurrency, afterItemCb = f => f) {
         this.concurrency = initConcurrency
         this.afterItemCb = afterItemCb
+
         this.processImportItem = this.makeCancellable(processImportItem)
     }
 
     set concurrency(value) {
         if (value > 0 && value <= ImportProgressManager.CONCURR_LIMIT) {
             this._concurrency = value
+            // Update Promise concurrency affording functionality
+            this.runConcurrent = promiseLimit(value)
         }
+    }
+
+    /**
+     * Allow token index state to loop back around from 0 to `this._concurrency`.
+     */
+    nextTokenIndex() {
+        if (this._tokenIndex === this._concurrency - 1) {
+            this._tokenIndex = 0
+        } else {
+            this._tokenIndex += 1
+        }
+
+        return this._tokenIndex
+    }
+
+    /**
+     * Updates token state to add another cancellable token for a running job.
+     *
+     * @param {Function} cancelCb Callback to run when all jobs are cancelled.
+     */
+    bindNextCancellableToken(cancelCb) {
+        this.tokens[this.nextTokenIndex()] = { cancel: cancelCb }
     }
 
     /**
@@ -37,10 +69,13 @@ class ImportProgressManager {
      */
     async start() {
         // Iterate through data chunks from the state manager
-        for await (const chunkData of stateManager.getItems()) {
+        for await (const { chunk, chunkKey } of stateManager.getItems()) {
             try {
-                // Run the chunk processor on the current data, passing in needed state
-                await this.processChunk(chunkData)
+                const importItemEntries = Object.entries(chunk)
+                const processEntry = this.processItem(chunkKey)
+
+                // For each chunk, run through the import item entries at specified level of concurrency
+                await this.runConcurrent.map(importItemEntries, processEntry)
             } catch (err) {
                 // If execution cancelled break Iterator processing
                 if (err.cancelled) {
@@ -51,21 +86,33 @@ class ImportProgressManager {
         }
     }
 
+    /**
+     * Goes through each Promise token and runs the `cancel()` callback, passing in Error
+     * with `cancel` bool property. Token state is cleared afterwards.
+     */
     stop() {
-        if (typeof this.token.cancel === 'function') {
-            // Create error with `cancelled` bool property to distinguish in callers
-            const err = new Error('Stopping progress')
-            err.cancelled = true
+        // Create error with `cancelled` bool property to distinguish in callers
+        const err = new Error('Stopping progress')
+        err.cancelled = true
 
-            // Run token's cancal callback to stop running async logic
-            this.token.cancel(err)
+        let token
+        while (this.tokens.length) {
+            token = this.tokens.pop()
+            if (token != null && typeof token.cancel === 'function') {
+                // Run token's cancal callback to stop running async logic
+                token.cancel(err)
+            }
         }
     }
 
+    /**
+     * @param {(a: any) => Promise<any>} asyncFn Async function to make cancellable
+     * @returns {(a: any) => Promise<any>} asyncFn Same function which will be interrupted upon `stop()` method call.
+     */
     makeCancellable = asyncFn => (...args) =>
         new Promise((resolve, reject) => {
             // Bind the reject callback to token to allow outside cancellation of `this` Promise
-            this.token.cancel = reject
+            this.bindNextCancellableToken(reject)
 
             // Run orig async function
             asyncFn(...args)
@@ -74,22 +121,27 @@ class ImportProgressManager {
         })
 
     /**
-     * @param {any} chunkData The chunk of import item state that is currently being processed
+     * @param {string} chunkKey The key of the chunk currently being processed.
+     * @returns {(chunkEntry) => Promise<void>} Async function affording processing of single entry in chunk.
      */
-    async processChunk({ chunk, chunkKey }) {
-        for (const [encodedUrl, importItem] of Object.entries(chunk)) {
-            let status, url, error
-            try {
-                const res = await this.processImportItem(importItem)
-                status = res.status
-            } catch (err) {
-                // Throw execution was cancelled, throw error up the stack
-                if (err.cancelled) {
-                    throw err
-                }
+    processItem = chunkKey => async ([encodedUrl, importItem]) => {
+        let status, url, error
+        let cancelled = false
+
+        try {
+            const res = await this.processImportItem(importItem)
+            status = res.status
+        } catch (err) {
+            // Throw execution was cancelled, throw error up the stack
+            if (err.cancelled) {
+                cancelled = true
+                throw err
+            } else {
                 error = err.message
-            } finally {
-                // Send item data + outcome status down to UI (and error if present)
+            }
+        } finally {
+            // Send item data + outcome status down to UI (and error if present)
+            if (!cancelled) {
                 this.afterItemCb({
                     type: importItem.type,
                     url,
